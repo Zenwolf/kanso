81c432a8f22d3a9e18d379af805c89595690d5d5
Matthew Jaquish <mattjaq@gmail.com>
2013-12-07 17:56:45 -0800

    Implemented identity map.

--- src/identity-map.js
+++ src/identity-map.js
@@ -1,144 +1,223 @@
-// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-// Copyright 2012, 2013 Matthew Jaquish
-// Licensed under The MIT License
-// http://opensource.org/licenses/MIT
-// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-
-/*
+(function () {
+/**
  * The identity map is a special kind of object that relies on a private map
  * with privileged methods to secure the resource instances.
  */
-define(function () {
 
-    var module = {};
+var type = kokou.Type;
 
-    // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-    // Factory function.
-    // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+/**
+ * An identity map that manages resources via priviledged methods that are the
+ * only methods with access to a private resource map.
+ */
+core.Class('kanso.IdentityMap', {
 
-    function initIMap(config) {
-        config = config || {};
-        this.data = this.data || {};
-        this.data.resourceCtor = config.resourceCtor || null;
-    }
+    events: {
+        RESOURCE_CREATED:   'RESOURCE_CREATED',
+        RESOURCE_DESTROYED: 'RESOURCE_DESTROYED',
+        RESOURCE_ADDED:     'RESOURCE_ADDED',
+        RESOURCE_REMOVED:   'RESOURCE_REMOVED'
+    },
 
-    /*
-     * config.resourceCtor {Function} Resource creation function.
+    /**
+     * Takes a @resourceCtor {Function} that is used as the constructor to
+     * create new resources. Also accepts an @emitter {kokou.Emitter?} that
+     * it can use to publish events.
      */
-    function create(config) {
-        var obj = {};
-        var map = {};
+    construct: function (resourceCtor, emitter) {
+        // Private map of resource instances.
+        var resourceMap = core.Main.createDict();
 
-        initIMap.call(obj, config);
+        this.__resourceCtor = resourceCtor;
+        this.__emitter = emitter || null;
 
         // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-        // Privileged methods.
+        // Priviledged methods that need to access the resource map.
         // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-        /*
-         * Get resource by id.
+        /**
+         * {kanso.Resource} Get a resource via its @id {String}.
          */
-        obj.get = function (id) {
-            var resource = map[id] || null;
+        this.get = function (id) {
+            var resource = null;
 
-            if (resource) {
+            // If there is no id, return the default.
+            if ( id === undefined ) {
                 return resource;
             }
 
-            if ( typeof id !== 'string' ) {
-                id += ''; // convert to string
+            // Convert the id to a string if it is not already a string.
+            id += '';
+
+            resource = resourceMap[id];
+
+            /*
+             * If there was no resource, we create a ghost object to allow the
+             * system to proceed. Ghost objects are filled in later after their
+             * data is populated.
+             */
+            if ( !resource ) {
+                resource = this.__create(id);
+                this.add(resource);
             }
 
-            // Just return a ghost object for now.
-            resource = this.create(id);
-            this.add(resource);
             return resource;
         };
 
-        /*
-         * Get a list of resources from an array of ids.
+        /**
+         * {Array[kanso.Resource]} Get a list of resources from an @ids {Array}.
          */
-        obj.getList = function (ids) {
+        this.getList = function (ids) {
             var resources = [];
 
+            if ( jasy.Env.isSet('debug') ) {
+                if ( !type.isArray(ids) ) {
+                    throw new Error('ids must be type array.');
+                }
+            }
+
             ids.forEach(function (id) {
-                var resource = this.get(id);
-                resources.push(resource);
+                resources.push( this.get(id) );
             }, this);
 
             return resources;
         };
 
-        /*
-         * Get all resources.
+        /**
+         * {Array[kanso.Resource]} Get all currently loaded resources.
          */
-        obj.getAll = function () {
+        this.getAll = function () {
             var resources = [];
 
             Object.keys(map).forEach(function (key) {
-                resources.push( map[key] );
+                resources.push( resourceMap[key] );
             });
 
             return resources;
         };
 
-        /*
-         * Whether or not the resource exists in the map.
+        /**
+         * {Boolean} Whether or not the resource exists in the map.
          */
-        obj.exists = function (id) {
-            return !!map[id];
-        };
-
-        /*
-         * Create a new empty resource using the resource creator function.
-         */
-        obj.create = function (id) {
-            var resource = this.data.resourceCtor({ id: id });
-            return resource;
+        this.exists = function (id) {
+            return !!resourceMap[id + ''];
         };
 
         /**
-         * Add a resource to the map.
+         * {Boolean} Add a @resource {kanso.Resource} to the map.
          */
-        obj.add = function (resource) {
-            var id = resource.get('id');
+        this.add = function (resource) {
+            // Make sure it is a string.
+            var id = resource.get('id') + '';
 
             if ( this.exists(id) ) {
+                if ( jasy.Env.isSet('debug') ) {
+                    console.warn('Resource already exists: ' + id);
+                }
+
                 return false;
             }
 
-            map[id] = resource;
+            resourceMap[id] = resource;
 
             return true;
         };
 
         /**
-         * Remove a resource by id from the map.
+         * {kanso.Resource} Remove a resource from the map by @id {String}.
          */
-        obj.remove = function (id) {
+        this.remove = function (id) {
+            var events = null;
             var resource = null;
 
-            if ( this.exists(key) ) {
-                resource = map[key];
-                delete map[key];
+            id += '';
+
+            if ( this.exists(id) ) {
+                resource = resourceMap[id];
+                delete resourceMap[id];
+
+                if ( this.__emitter ) {
+                    events = core.Class.getEvents( kanso.IdentityMap );
+
+                    this.__emitter.emit( e.RESOURCE_REMOVED,
+                        this.__createResourceRemovedEvent(resource) );
+                }
             }
 
             return resource;
         };
 
-        obj.clear = function () {
-            map = {};
+        /**
+         * Clears the map of all resources.
+         */
+        this.clear = function () {
+            resourceMap = core.Main.createDict();
         };
 
-        return obj;
+        /**
+         * {kanso.Resource} Create a new ghost(unpopulated) resource with the
+         * given @id {String}.
+         */
+        this.__create = function (id) {
+            var resource = null;
+            var events = null;
+
+            id += '';
+
+            resource = new this.__resourceCtor({ id: id });
+
+            if ( this.__emitter ) {
+                events = core.Class.getEvents(kanso.IdentityMap);
+
+                this.__emitter.emit( events.RESOURCE_CREATED,
+                    this.__createResourceCreatedEvent(resource) );
+            }
+
+            return resource;
+        };
+    },
+
+    members: {
+        /**
+         * {kokou.Emitter} Get the current emitter.
+         */
+        getEmitter: function () {
+            return this.__emitter;
+        },
+
+        /**
+         * Set the @mitter {kokou.Emitter} that will be used to publish events.
+         */
+        setEmitter: function (emitter) {
+            this.__emitter = emitter;
+        },
+
+        /**
+         * {Object} Use the @resource {kanso.Resource} to create an event.
+         */
+        __createResourceCreatedEvent: function (resource) {
+            var events = core.Class.getEvents(kanso.IdentityMap);
+
+            return this.__createEvent(events.RESOURCE_CREATED, resource);
+        },
+
+        /**
+         * {Object} Use the @resource {kanso.Resource} to create an event.
+         */
+        __createResourceRemovedEvent: function (resource) {
+            var events = core.Class.getEvents(kanso.IdentityMap);
+
+            return this.__createEvent(events.RESOURCE_REMOVED, resource);
+        },
+
+        __createEvent: function (type, resource) {
+            return {
+                type: type,
+                timestamp: +(new Date()),
+                resource: resource
+            };
+        }
     }
-
-
-    // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-    // Public module.
-    // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-
-    module.create = create;
-
-    return module;
 });
+
+} ());
